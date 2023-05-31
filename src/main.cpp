a135d0c1d6ab5d148cae9d9eefbce3d00cef3ac5
Dennis Schwartze <dennis.schwartze@gmail.com>
2023-05-31 11:28:37 +0200

    one json method loads everything

--- src/main.cpp
+++ src/main.cpp
@@ -3,44 +3,53 @@
 #include <imgui_stdlib.h>
 #include <fstream>
 #include <iostream>
+#include <filesystem>
 
 
-nlohmann::json load_json(const std::string path){
+
+
+
+nlohmann::json load_json_file(const std::string path){
     std::ifstream json_file(path);
     nlohmann::json return_json;
     json_file>>return_json;
     return return_json; 
-
+}
+void write_json_file(const std::string path, const nlohmann::json& json_data) {
+    std::ofstream json_file(path);
+    json_file << json_data.dump(4);
+    json_file.close();
 }
 template<typename T>
-T load_config(const std::string key){
-    nlohmann::json settings = load_json("../../config/config.json");
-    return settings[key].get<T>();
+T load_json(nlohmann::json configjson, std::string key){
+    return configjson[key].get<T>();
 }
 template<typename T>
-T load_config(const std::string firstkey, const std::string lastkey){
-    nlohmann::json settings = load_json("../../config/config.json");
-    return settings[firstkey][lastkey].get<T>();
+T load_json(nlohmann::json configjson, std::string key1, std::string key2){
+    return configjson[key1][key2].get<T>();
+}
+template<typename T>
+T load_json(nlohmann::json configjson, std::string key1, std::string key2, std::string key3){
+    return configjson[key1][key2][key3].get<T>();
+}
+template<typename T>
+T load_json(nlohmann::json configjson, std::string key1, std::string key2, std::string key3,std::string key4){
+    return configjson[key1][key2][key3][key4].get<T>();
 }
 
-
-auto load_lang_string(const std::string& lang_key) {
-    std::ifstream lang_file("../../config/" + load_config<std::string>("language")+ ".json");
-    nlohmann::json lang_settings;
-    lang_file >> lang_settings;
-    std::string returnstring = lang_settings[lang_key].get<std::string>();
-    std::cout << returnstring << std::endl;
-    return returnstring;
+std::vector<std::string> getAvailableLanguages(const std::string& languageFolder) {
+    std::vector<std::string> languages;
+    for (const auto& entry : std::filesystem::directory_iterator(languageFolder)) {
+        if (entry.is_regular_file()) {
+            std::string extension = entry.path().extension().string();
+            if (extension == ".json") {
+                std::string filename = entry.path().filename().stem().string();
+                languages.push_back(filename);
+            }
+        }
+    }
+    return languages;
 }
-
-
-struct label{
-    std::string input_text_fin_label = load_lang_string("input_text_fin_label");
-    std::string input_text_fin_fail = load_lang_string("input_text_fin_fail");
-    std::string input_text_fin_complete = load_lang_string("input_text_fin_complete");
-    std::string button_save_label = load_lang_string("button_save_label");
-};
-
 
 
 template <typename T>
@@ -67,26 +76,38 @@ void save_data(const std::string& fin) {
 }
 
 int main() {
-    label mylabel;
-    bool showOptionsWindow = true;   
-    //std::string input_text_fin_label = {load_lang_string("input_text_fin_label")};
-
+    nlohmann::json config = load_json_file("../../config/config.json");
+    nlohmann::json language = load_json_file("../../config/" + load_json<std::string>(config,"language") + ".json");
+    
     auto render = [&]() {
+
+
         ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
         ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
         ImGui::Begin("foobar", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoResize);
         ImGui::SetWindowFontScale(3);
         
-        if (ImGui::BeginMenu("Menu")) {
+        if (ImGui::BeginMenu(load_json<std::string>(language,"menu","label").c_str())) {
             // Optionen hinzufügen
-            if (ImGui::MenuItem("Optionen")) {
-                showOptionsWindow = true;
+            if (ImGui::BeginMenu(load_json<std::string>(language,"menu","option").c_str())) {
+                ImGui::SetWindowFontScale(3);
+                if (ImGui::MenuItem("Deutsch")) {
+                    config["language"]="de";
+                    write_json_file("../../config/config.json",config);
+                }
+                if (ImGui::MenuItem("Englisch")) {
+                    config["language"]="en";
+                    write_json_file("../../config/config.json",config);
+                }
+                
+                ImGui::EndMenu();    
             }
+            
             if (ImGui::MenuItem("nope")) {
-                // Aktion für Option 2
+           
             }
             if (ImGui::MenuItem("no overtime")) {
-                // Aktion für Option 3
+              
             }
             
             ImGui::EndMenu();
@@ -95,33 +116,22 @@ int main() {
         static char fahrzeugidentnummer[18]="";
         
         //ImGui::InputText("input_text_fin_label", fahrzeugidentnummer,18);
-        ImGui::InputText(mylabel.input_text_fin_label.c_str(), fahrzeugidentnummer,18);
+        ImGui::InputText(load_json<std::string>(language,"inputfin","label").c_str(), fahrzeugidentnummer,18);
         if(check_fin_valid(fahrzeugidentnummer)){
-            ImGui::Text(mylabel.input_text_fin_complete.c_str());
+            ImGui::Text(load_json<std::string>(language,"inputfin","fail").c_str());
         } else {
-            ImGui::Text(mylabel.input_text_fin_fail.c_str());
+            ImGui::Text(load_json<std::string>(language,"inputfin","complete").c_str());
         }
-                
-        ImGui::Button(mylabel.button_save_label.c_str(), ImVec2(load_config<int>("button","sizex"), load_config<int>("button","sizey")));
+             
+        ImGui::Button(load_json<std::string>(language,"button","save","label").c_str(), ImVec2(load_json<int>(config,"button","sizex"), load_json<int>(config,"button","sizey")));
 
-
+        
         
 
         ImGui::End();
-
-        if (showOptionsWindow) {
-            ImGui::Begin("Options", nullptr, ImGuiWindowFlags_NoSavedSettings);
-            ImGui::SetWindowFontScale(3);
-            // Hier kannst du die Optionen für das Optionsfenster hinzufügen
-            // ...
-
-            ImGui::End();
-        }
-        
-
     };
    
-    ImGuiInstance window{load_config<size_t>("window","sizex"), load_config<size_t>("window","sizey"), "Test"};
+    ImGuiInstance window{1920,1080, "Test"};
 
     while(window.run(render)) {
         
